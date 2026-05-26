9cbea1b53e7065f64cdc94c73e81c334c579c68c
Bartłomiej Ćwiklak <62007413+bartlomiejcwiklak@users.noreply.github.com>
2026-05-26 02:32:39 +0200

    Remove native TV files from web branch

--- android/app/src/main/java/com/baroflix/app/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package com.baroflix.app;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.baroflix.app',
-  appName: 'baroflix',
-  webDir: 'dist'
-};
-
-export default config;
